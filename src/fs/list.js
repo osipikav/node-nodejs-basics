0e7db33e82b140ed776ad582dbdcda9345d4bfc2
Andrei Asipik <osipik.av@yandex.by>
2024-01-25 23:37:41 +0300

    feat: complited list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
-const list = async () => {
-    // Write your code here 
-};
+import path from "node:path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import fs from "node:fs";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const list = async () => {
+  const filePath = path.join(__dirname, "files");
+  fs.readdir(filePath, (err, files) => {
+    if (err) throw new Error("FS operation failed");
+    console.log(files);
+  });
+};
 await list();
