427c2791c4ed1b32e4a547d512217275438f05af
Andrei Asipik <osipik.av@yandex.by>
2024-01-25 23:01:47 +0300

    feat: complited copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,35 @@
+import path from "node:path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import fs from "node:fs";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const data = "I am fresh and young";
+
 const copy = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, "files");
+  const finalFilePath = path.join(__dirname, "files_copy");
+
+  fs.readdir(__dirname, (err, files) => {
+       if (files.includes("files_copy")) {
+        throw new Error ("FS operation failed");
+    }
+  fs.readdir(filePath, (err, files) => {
+    if (err) throw new Error("FS operation failed");
+
+    fs.mkdir("./files_copy", () => {
+      files.forEach((i) => {
+        fs.copyFile(
+          path.join(filePath, i),
+          path.join(finalFilePath, i),
+          (err) => {
+            if (err) throw err
+          }
+        );
+      });
+    });
+  });
+  });
 };
 
 await copy();
