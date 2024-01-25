99aa13de3b795dc255bb9c6ca6b0d58040d9f1c8
Andrei Asipik <osipik.av@yandex.by>
2024-01-25 23:57:49 +0300

    feat: complited rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,19 @@
+import path from "node:path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import fs from "node:fs";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const rename = async () => {
-    // Write your code here 
+    const folderPath = path.join(__dirname, "files");
+    
+    fs.rename(
+        path.join(folderPath, 'wrongFilename.txt'),
+        path.join(folderPath, "properFilename.md"),
+        (err) => {
+            if (err) throw new Error("FS operation failed");
+        },)
 };
 
 await rename();
