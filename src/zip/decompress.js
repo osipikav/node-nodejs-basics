98f6292cdec8ba695b16f91c944b9cd5d05bda0e
Andrei Asipik <osipik.av@yandex.by>
2024-01-29 00:44:55 +0300

    feat: complited decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import path from "node:path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { createGunzip } from 'zlib';
+import fs from "node:fs";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const decompress = async () => {
-    // Write your code here 
+  const compressFilePath = path.join(__dirname, "files", "fileToCompress.txt");
+  const archiveFilePath = path.join(__dirname, "files", "archive.gz");
+
+  const readStream = fs.createReadStream(archiveFilePath);
+  const writeStream = fs.createWriteStream(compressFilePath);
+  const gunzipStream = createGunzip();
+  readStream.pipe(gunzipStream).pipe(writeStream);
 };
 
 await decompress();
