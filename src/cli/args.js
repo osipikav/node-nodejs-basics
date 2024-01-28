6f78e34788a900166e0b9cc244676ac031610f1c
Andrei Asipik <osipik.av@yandex.by>
2024-01-28 20:08:34 +0300

    feat: complited args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+  let result = "";
+  const args = process.argv.slice(2);
+  args.forEach((el, i) => {
+    if (i % 2 === 0) {
+      result = result + el.slice(2) + " is ";
+    } else if (i < args.length - 1) {
+      result = result + el + ", ";
+    } else {
+      result = result + el;
+    }
+  });
+
+  console.log(result);
 };
 
 parseArgs();
