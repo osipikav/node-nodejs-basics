daa262448b7283fa1571d14e4805ab037a40c019
Andrei Asipik <osipik.av@yandex.by>
2024-01-28 22:56:01 +0300

    feat: complited read.js and write.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import path from "node:path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import fs from "node:fs";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+    const filePath=path.resolve(__dirname,'files', "fileToRead.txt");
+    const stream = fs.createReadStream(filePath, "utf-8");
+    stream.pipe(process.stdout);
+  
 };
 
 await read();
