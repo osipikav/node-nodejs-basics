aedee4a392a80a7dedb77332fb216d4cfa66ad4c
Andrei Asipik <osipik.av@yandex.by>
2024-01-30 01:50:06 +0300

    feat: complited worker.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,15 @@
+import path from "node:path";
+import { Worker } from "node:worker_threads";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const performCalculations = async () => {
-    // Write your code here
+  const pathWorker = path.join(__dirname, "worker.js");
+  const worker = new Worker(pathWorker, { workerData: 10 });
+  worker.on("message", (result) => {
+    console.log(result);
+  });
 };
 
 await performCalculations();
